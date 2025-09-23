c540ecb781d469c98966e53b6899cbb9c98766f9
promes27 <franchescopromes@gmail.com>
2025-09-23 11:41:03 +0300

    front

---
+++ pages/6_Fournisseurs.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from datetime import date
+
+# Sidebar
+with st.sidebar:
+    st.sidebar.image("images/logoMahein.png", caption="", use_container_width=True)
+
+# Charger le fichier CSS
+with open("./style/style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+with st.container():
+    # Création des colonnes
+    col1, col2 = st.columns([5, 2])  # largeur proportionnelle (ajuste si besoin)
+
+    with col1:
+        st.markdown('<h1 class="title">Fournisseurs</h1>', unsafe_allow_html=True)
+
+    with col2:
+        date_min_possible = date(2023, 1, 1)
+        date_max_possible = date(2025, 12, 31)
+
+        plage_dates = st.date_input(
+            "",
+            value=(date(2024, 1, 1), date(2024, 12, 31)),  
+            min_value=date_min_possible,
+            max_value=date_max_possible
+        )
